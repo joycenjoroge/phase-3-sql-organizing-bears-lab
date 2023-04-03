019c3af7e9995ea6852363229152a19a1b1933cc
Joyce <joycenjoroge47@gmail.com>
2023-04-03 07:39:11 +0300

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate", 5,"M","Brown","High",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy", 8,"M","Black","Medium",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha", 7,"F","White","High",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown", 4,"M","Brown","Low",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa", 4,"F","Brown","High",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch", 10,"M","Black","High",0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy", 6,"F","White","High",1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL, 5,"M","Brown","High",1);
